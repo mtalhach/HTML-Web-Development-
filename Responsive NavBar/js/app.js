9f6d1283ed5766da67baea75df72717d10bc3077
CH TALHA <100463587+mtalhach@users.noreply.github.com>
2022-08-01 12:13:43 +0500

    HTML

--- Responsive NavBar/js/app.js
+++
@@ -1,5 +0,0 @@
-const toggleButton = document.getElementsByClassName('toggle_button')[0]
-const navbatlinks = document.getElementsByClassName('navbar_links')[0]
-toggleButton.addEventListener('click', () => {
-    navbatlinks.classList.toggle('active')
-})
